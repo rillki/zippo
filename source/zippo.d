7e3f70a8b1e6b2e59bffbb237435a0c365e286ba
Ki Rill <rillk500@gmail.com>
2021-03-18 13:12:03 +0600

    changed input arguments:

--- source/zippo.d
+++ source/zippo.d
@@ -4,7 +4,35 @@ import ziputil;
 
 import std.stdio: writeln, writefln;
 import std.algorithm.searching: canFind;
-import std.algorithm.mutation: remove;
+
+/*
+# ZIP
+: zippo zip
+    zip all files in cwd
+: zippo zip path=/Desktop
+    zip all files in '/Desktop'
+: zippo zip name=flower.zip
+    zip all files in cwd, name the zip file as 'flower.zip'
+: zippo zip file=flower1.png,flower2.jpg,flowers.txt
+    zip only 'flower1.png', 'flower2.jpg' and 'flowers.txt'
+: zippo zip ignore=flower7.png,flower3.jpg
+    zip all files in cwd excluding 'flower7.png' and 'flower3.jpg'
+
+# UNZIP
+: zippo unzip
+    unzip all *.zip files in cwd
+
+# LIST
+: zippo list
+    list contents of all *.zip files in cwd
+: zippo list path=/Desktop/zipfile.zip
+    list contents of 'zipfile.zip' located at '/Desktop'
+: zippo list path=/Desktop name=zipfile.zip
+    list contents of 'zipfile.zip' located at '/Desktop'
+
+
+*/
+
 
 /++ a list of all available commands
 +/
@@ -12,50 +40,52 @@ enum Commands {
     Name = "name",
     Path = "path",
     File = "file",
-    Zip = "zip",
     Ignore = "ignore",
-    All = "all"
 }
 
 /++ handles the zip utility's logic
 +/
 void zippoUtility(const(string[]) args) {
+    import std.array: empty;
+
     immutable string help = "\n/******* Zippo: a command line ZIP utility *******/\n\n" ~
                             "<OPTIONS>:\n" ~
-                            "\tzip/unzip/list \t\t\tto zip, unzip or list zipfile contents\n\n" ~
-                            "\t\t\t\t\t [default: list]\n" ~
+                            "\tzip/unzip/list \t\t\tto zip, unzip or list zipfile contents\n" ~
+                            "\t-h/--help \t\t\tprint man page\n" ~ 
+                            "\t-v/--verbose \t\t\tverbose mode, output the entire process\n\n" ~ 
                             "<WHERE AND WHAT>:\n" ~ 
                             "\tpath=\"<...>\" \t\t\tspecify a directory, a file(s) to be ziped or unziped\n" ~
                             "\t\t\t\t\t [default: cwd is used]\n" ~
                             "\tfile=\"file1,file2,fileX\" \tuse the \",\" comma to zip/unzip specified files only\n" ~
                             "\t\t\t\t\t [default: includes all files]\n" ~
                             "\tignore=\"file1|file2,fileX\" \tuse the \",\" comma to exclude certains files\n" ~
-                            "\t\t\t\t\t [default: none are ignored]\n";
+                            "\t\t\t\t\t [default: none are ignored]\n"; 
 
     // display help manual
-    if(args.length < 2 || args[1] == "-h" || args[1] == "--help") {
+    if(args.length < 2 || args[1] == "-h" || args[1] == "--help" || args[1] == "-v" || args[1] == "--verbose") {
         writeln(help);
         return;
     }
     
     // find what should it do: zip, unzip or list zipfile contents
-    immutable(string) action = args.canFind("zip") ? ("zip") : (args.canFind("unzip") ? ("unzip") : ("list"));
-    auto newArgs = (args.dup).remove!(a => a == action);
+    immutable(string) action = args.canFind("zip") ? ("zip") : (args.canFind("unzip") ? ("unzip") : (args.canFind("list") ? ("list") : ("")));
+    if (action.empty()) {
+        writeln("\n# Action wasn't specified! [zip/unzip/list]");
+        writeln("# Use -h or --help for more information.\n");
+        return;
+    }
 
     // get command line arguments
-    string[string] info = getCommandLineArguments(newArgs);
-
-    // configure command line arguments (what is not specified will be set to the default value)
-    // TODO: start from here, simplify
-    info.configureCommanLineArguments();
+    immutable(string[string]) info = getCommandLineArguments(args);
+    writeln(info);
 
     // notify the user that the process has begun
     writeln("---------- PROCESS STARTED! ----------");
 
     // check if we should zip, unzip or list zip contents
-    if(info[Commands.Zip] == "y") {
+    if(action == "zip") {
         compressUtility(info);
-    } else if(info[Commands.Zip] == "n") {
+    } else if(action == "unzip") {
         decompressUtility(info);
     } else {
         listZipContents(info[Commands.Name], info[Commands.Path]);
@@ -71,40 +101,23 @@ void zippoUtility(const(string[]) args) {
     out:
         string[string]
 +/
-string[string] getCommandLineArguments(const(string[]) args) {
-    import std.algorithm: findSplit;
+immutable(string[string]) getCommandLineArguments(const(string[]) args) {
+    import std.algorithm: findSplit, canFind;
+    import std.file: getcwd;
 
-    string[string] info;
+    string[string] info = [
+        Commands.Name : "zipfile.zip",
+        Commands.Path : getcwd(),
+        Commands.File : "none",
+        Commands.Ignore : "none",
+    ];
+
     for(size_t i = 1; i < args.length; i++) {
         auto s = findSplit(args[i], "=");
         info[s[0]] = s[2];
     }
 
-    return info;
-}
-
-/++ configures command line arguments to the default values if the value is not specified by the user
-    in:
-        string[string] info
-+/
-void configureCommanLineArguments(string[string] info) {
-    import std.algorithm: findSplit, canFind;
-
-    if((Commands.Zip in info) is null) {
-        info[Commands.Zip] = Defaults.Zip;
-    }
-
-    if((Commands.Name in info) is null) {
-        info[Commands.Name] = Defaults.Name;
-    }
-
-    if((Commands.Path in info) is null) {
-        info[Commands.Path] = Defaults.Path;
-    }
-
-    if((Commands.Ignore in info) is null) {
-        info[Commands.Ignore] = Defaults.Ignore;
-    }
+    return cast(immutable)(info);
 }
 
 /++ compresses the data into a zip file
@@ -113,7 +126,7 @@ void configureCommanLineArguments(string[string] info) {
 +/
 void compressUtility(const(string[string]) info) {
     // if the defaults are chosen, compress all files in the current directory and save to ZIP_FILE.zip
-    if((Commands.All in info) !is null) {
+    /*if((Commands.All in info) !is null) {
         compressAll;
     } else {
         // if files are not specified, then compress all files in a directory excluding the specified files, if provided
@@ -126,7 +139,7 @@ void compressUtility(const(string[string]) info) {
         } else {
             compress(info[Commands.Name], info[Commands.Path], info[Commands.File].multipleSplit("|"));
         }
-    }
+    }*/
 }
 
 /++ decompresses zipped data
