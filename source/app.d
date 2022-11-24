a0955c053edfdb22834e2c74c603233184d1e656
Ki Rill <rill.ki@yahoo.com>
2022-11-24 22:10:10 +0600

    refactoring

--- source/app.d
+++ source/app.d
@@ -1,11 +1,47 @@
 module main;
 
-// main Zippo module
-import zinterface: app;
+enum ZIPPO_VERSION = "0.1.0";
 
 void main(string[] args) {
-    app(args);
+    if(args.length < 2) {
+        writefln("#zippo: no commands provided! See \'zippo help\' for more info.");
+        return;
+    }
+
+    // check case
+    switch(args[1]) {
+        case "z":
+        case "zip":
+            break;
+        case "u":
+        case "unzip":
+            break;
+        case "l":
+        case "list":
+            break;
+        case "v":
+        case "version":
+            import std.compiler: version_major, version_minor;
+            writefln("zippo version %s - A simple command line ZIP utility.", ZIPPO_VERSION);
+            writefln("Built with %s v%s.%s on %s", __VENDOR__, version_major, version_minor, __DATE__);
+            break;
+        case "h":
+        case "help":
+            writefln("zippo version %s - A simple command line ZIP utility.", ZIPPO_VERSION);
+            writefln("z     zip [OPTIONS]  initializes a new database");
+            writefln("u   unzip [OPTIONS]  removes an existing database");
+            writefln("l    list [OPTIONS]  switches to the specified database");
+            writefln("v version            display current version");
+            writefln("h    help            This help manual.\n");
+            writefln("EXAMPLE: zippo zip ");
+            break;
+        default:
+            writefln("#zippo: Unrecognized option %s!", args[1]);
+            break;
+    }
 }
 
-
+void zippoZip() {}
+void zippoUnzip() {}
+void zippoList() {}
 
--- source/zinterface.d
+++ source/zinterface.d
@@ -11,9 +11,7 @@ import std.getopt: getopt, GetoptResult, defaultGetoptPrinter;
 // Zippo module containing functions for working with *.zip files
 import zutil = zutility;
 
-enum ZIPPO_VERSION = "0.1.0";
-
-/++ // std commands
+// std commands
 enum Commands: string {
     zip = "zip",
     unzip = "unzip",
@@ -36,47 +34,10 @@ enum Options: string {
     ignoreLong = "--ignore",
 
     none = "none",
-} +/
+}
 
 void app(string[] args) {
-    if(args.length < 2) {
-        writefln("#zippo: no commands provided! See \'zippo help\' for more info.");
-        return;
-    }
-
-    // check case
-    switch(args[1]) {
-        case "z":
-        case "zip":
-            break;
-        case "u":
-        case "unzip":
-            break;
-        case "l":
-        case "list":
-            break;
-        case "v":
-        case "version":
-            import std.compiler: version_major, version_minor;
-            writefln("zippo version %s - A simple command line ZIP utility.", ZIPPO_VERSION);
-            writefln("Built with %s v%s.%s on %s", __VENDOR__, version_major, version_minor, __DATE__);
-            break;
-        case "h":
-        case "help":
-            writefln("zippo version %s - A simple command line ZIP utility.", ZIPPO_VERSION);
-            writefln("z     zip [OPTIONS]  initializes a new database");
-            writefln("u   unzip [OPTIONS]  removes an existing database");
-            writefln("l    list [OPTIONS]  switches to the specified database");
-            writefln("v version            display current version");
-            writefln("h    help            This help manual.\n");
-            writefln("EXAMPLE: zippo zip ");
-            break;
-        default:
-            writefln("#zippo: Unrecognized option %s!", args[1]);
-            break;
-    }
-
-    /++ // quick overview 
+    // quick overview 
     immutable string manpage = "\nOVERVIEW: Zippo - the (de)compression utility v2.0\n" ~
                 "\nUSAGE: zippo [command] [options]\n" ~
                 "\nCOMMANDS:" ~
@@ -158,10 +119,8 @@ void app(string[] args) {
         default:
             writef("%s\n", manpage);
             return;
-    }+/
+    }
 }
 
-void zippoZip() {}
-void zippoUnzip() {}
-void zippoList() {}
+
 
